fcb724870a8ccfaceecdd91b25019f774a84c8eb
zqy <850074816@qq.com>
2019-03-11 15:16:57 +0800

    change some txt to bash

--- ubuntu/dock.sh
+++ ubuntu/dock.sh
@@ -1,5 +1,5 @@
+#!/bin/sh
 
-
-ubuntu 16.04 dock
+#ubuntu 16.04 dock
 
 gsettings set com.canonical.Unity.Launcher launcher-position Bottom
---
+++ vscode/ttf.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+sudo apt-get install ttf-mscorefonts-installer
+
+#sudo apt install ttf-mscorefonts-installer 
