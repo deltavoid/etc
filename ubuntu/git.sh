2a26b5347426f9e437198f703ae71b827112b616
zhangqianyu.sys <zhangqianyu.sys@bytedance.com>
2020-01-01 01:45:08 +0800

    rm makeifle and widnows, add windows_terminal_profile

--- ubuntu/git.sh
+++ ubuntu/git.sh
@@ -3,7 +3,7 @@
 #git config --global user.name "zqy"
 #git config --global user.email "850074816@qq.com"
 
-git config --global user.name "zhangqianyu.sys"
+git config --global user.name  "zhangqianyu.sys"
 git config --global user.email "zhangqianyu.sys@bytedance.com"
 
 #git config --global push.default simple
