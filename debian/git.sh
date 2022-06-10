0a36db19740032d92ae5b12f6d20d68385009df5
zqy <850074816@qq.com>
2022-06-10 20:58:43 +0800

    update hostname

--- debian/git.sh
+++ debian/git.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-#git config --global user.name "zqy"
-#git config --global user.email "850074816@qq.com"
+git config --global user.name "zqy"
+git config --global user.email "850074816@qq.com"
 
-git config user.name  "zhangqianyu.sys"
-git config user.email "zhangqianyu.sys@bytedance.com"
+# git config user.name  "zhangqianyu.sys"
+# git config user.email "zhangqianyu.sys@bytedance.com"
 
 
 
